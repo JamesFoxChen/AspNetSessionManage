9d7c10b4fabc9e6e0382b2267697100177778a95
JamesFoxChen <25717594@qq.com>
2015-10-13 11:44:35 +0800

    add

---
+++ RedisTest/RedisConfig.cs
@@ -0,0 +1,62 @@
+﻿
+using ServiceStack.Redis;
+using System.Configuration;
+using System.Linq;
+
+namespace RedisTest
+{
+    internal class RedisConfig
+    {
+        public static RedisClient Redis
+        {
+            get
+            {
+                return (RedisClient)reidsPools.GetClient();
+            }
+        }
+
+
+        //public static RedisClient RedisDB6
+        //{
+        //    get
+        //    {
+        //        return (RedisClient)redisPools6.GetClient();
+        //    }
+        //}
+
+        private static string[] hosts;
+        private static PooledRedisClientManager reidsPools;
+        //private static PooledRedisClientManager reidsPools4;
+        //private static PooledRedisClientManager redisPools6;
+
+        static RedisConfig()
+        {
+            var servers = ConfigurationManager.AppSettings["RedisServer"].Split(',');
+            hosts = servers.Select(c => c + ":" + ConfigurationManager.AppSettings["RedisServerPort"]).ToArray();
+
+            reidsPools = new PooledRedisClientManager(hosts, hosts, new RedisClientManagerConfig
+            {
+                MaxWritePoolSize = 10,//“写”链接池链接数
+                MaxReadPoolSize = 10,//“写”链接池链接数
+                AutoStart = true,
+                DefaultDb = 0
+            });
+
+            //reidsPools4 = new PooledRedisClientManager(hosts, hosts, new RedisClientManagerConfig
+            //{
+            //    MaxWritePoolSize = 10,//“写”链接池链接数
+            //    MaxReadPoolSize = 10,//“写”链接池链接数
+            //    AutoStart = true,
+            //    DefaultDb = 4
+            //});
+
+            //redisPools6 = new PooledRedisClientManager(hosts, hosts, new RedisClientManagerConfig
+            //{
+            //    MaxWritePoolSize = 10,//“写”链接池链接数
+            //    MaxReadPoolSize = 10,//“写”链接池链接数
+            //    AutoStart = true,
+            //    DefaultDb = 6
+            //});
+        }
+    }
+}
---
+++ RedisTest/RedisHelper.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace RedisTest
+{
+    public class RedisHelper
+    {
+        /// <summary>
+        /// 获取值
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static T Get<T>(string key)
+        {
+            using (var redis = RedisConfig.Redis)
+            {
+                return redis.Get<T>(key);
+            }
+        }
+
+        /// <summary>
+        /// 设置值
+        /// </summary>
+        /// <ty.peparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool Set<T>(string key, T value)
+        {
+            using (var redis = RedisConfig.Redis)
+            {
+                return redis.Set<T>(key, value);
+            }
+        }
+
+        /// <summary>
+        /// 设置值
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool Set<T>(string key, T value, DateTime dt)
+        {
+            using (var redis = RedisConfig.Redis)
+            {
+                return redis.Set<T>(key, value, dt);
+            }
+        }
+
+        /// <summary>
+        /// 移除单个值
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static bool Remove(string key)
+        {
+            using (var redis = RedisConfig.Redis)
+            {
+                return redis.Remove(key);
+            }
+        }
+
+        /// <summary>
+        /// 移除集合值
+        /// </summary>
+        /// <param name="keys"></param>
+        public static void RemoveList(List<string> keys)
+        {
+            using (var redis = RedisConfig.Redis)
+            {
+                foreach (var key in keys)
+                {
+                    redis.Remove(key);
+                }
+            }
+        }
+    }
+}
