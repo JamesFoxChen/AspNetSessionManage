5f7dd3681f94df428e80b9b16fc5047615e3fbb4
JamesFoxChen <25717594@qq.com>
2015-10-20 14:08:08 +0800

    移除多余项目

---
+++ RedisCustom9000/RedisTest.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RedisCustom9000
+{
+    public partial class RedisTest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            RedisHelper.Set<string>("aa", DateTime.Now.ToString());
+            var str = RedisHelper.Get<string>("aa");
+        }
+    }
+}
